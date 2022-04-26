fa5fd3d8caf7eb3f4d0645295552f2ffbd412139
Aya Ramirez <ayaramz@gmail.com>
2022-04-26 00:26:25 -0400

    commit tree ea9296f23dc32f24ba81cc67185e8c4fbeda7b50

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let result
+  if (number < 400) {
+    result = 'This one is on me!'
+  } else if (number >= 400 && number <= 2000) {
+    result ='That will be twenty bucks.' 
+  } else if (number > 2000 && number <= 2500){
+    result = 'I will gladly take your thirty bucks.'
+  } else {
+    result = 'No can do.'
+  }
+  return result 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return cityName === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let phrase 
+  switch (tip){
+    case 'generous':
+      phrase = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      phrase = 'Thank you.'
+      break;
+    default:
+      phrase = 'Bye.'
+      break;
+  }
+  return phrase
 }
